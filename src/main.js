abfe415f6b2239cc51bcdfdb81aee65a57e10579
Student <example@example.com>
2019-03-11 14:30:44 -0700

    initial commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import './styles.css';
+import { Simon } from './simon.js';
+var simon  = new Simon();
+console.log(simon);
+
+// $(document).ready(function() {
+//
+//   $(".start").on("click", function(){
+//     $("#" + simon.randomColor()).addClass(".grow");
+//   });
+//
+//
+// });
---
+++ src/simon.js
@@ -0,0 +1,10 @@
+export function Simon() {
+  this.color = ["red", "yellow", "blue", "green"];
+  this.currentPattern = [];
+}
+// 
+// Simon.prototype.randomColor = function() {
+//   var randomNum = Math.floor((Math.random() * 3);
+//   return this.color[randomNum];
+//   //will return random Simon color as string
+// };
