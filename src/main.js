68eaf7ea9b21b1701fe799a4157bef46dd68b298
Student <example@example.com>
2019-03-11 16:43:14 -0700

    it will have playable in console simon game

--- src/main.js
+++ src/main.js
@@ -1,13 +1,25 @@
 import './styles.css';
 import { Simon } from './simon.js';
 var simon  = new Simon();
-console.log(simon);
 
-// $(document).ready(function() {
-//
-//   $(".start").on("click", function(){
-//     $("#" + simon.randomColor()).addClass(".grow");
-//   });
-//
-//
-// });
+$(document).ready(function() {
+
+  $(".start").on("click", function(){
+    simon.addToPattern();
+  });
+
+  $(".block").click(function(){
+    var userColor = $(this).attr("id");
+    simon.userPattern.push(userColor);
+    if(!simon.checkPattern()) {
+      simon.gameReset();
+      console.log("game over");
+    } else if (simon.userPattern.length === simon.gamePattern.length ) {
+          simon.addToPattern();
+          simon.userPattern = [];
+    }
+    console.log("user " + simon.userPattern);
+    console.log("game " + simon.gamePattern);
+  });
+
+});
--- src/simon.js
+++ src/simon.js
@@ -1,10 +1,34 @@
 export function Simon() {
   this.color = ["red", "yellow", "blue", "green"];
-  this.currentPattern = [];
+  this.gamePattern = [];
+  this.userPattern = [];
+  this.activePlayer = false;
+
 }
-// 
-// Simon.prototype.randomColor = function() {
-//   var randomNum = Math.floor((Math.random() * 3);
-//   return this.color[randomNum];
-//   //will return random Simon color as string
-// };
+
+Simon.prototype.randomColor = function() {
+  var randomNum = Math.floor((Math.random() * 4));
+  return this.color[randomNum];
+  //will return random Simon color as string
+};
+
+
+Simon.prototype.checkPattern = function() {
+  for(let i = 0; i < this.userPattern.length; i++) {
+    if(this.userPattern[i] !== this.gamePattern[i]) {
+      return false
+    }
+  }
+  return true
+};
+
+Simon.prototype.addToPattern = function (){
+  var randomColor = this.randomColor();
+  this.gamePattern.push(randomColor);
+  console.log(this.gamePattern);
+};
+
+Simon.prototype.gameReset = function () {
+  this.gamePattern = [];
+  this.userPattern = [];
+};
